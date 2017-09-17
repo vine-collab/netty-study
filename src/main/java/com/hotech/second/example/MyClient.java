a76ecd82558feb4789d3729538b12df498a20ed3
hekai <hekai0118@gmail.com>
2017-09-17 12:40:32 +0800

    second example socket demo

---
+++ src/main/java/com/hotech/second/example/MyClient.java
@@ -0,0 +1,25 @@
+package com.hotech.second.example;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+/**
+ * Created by hekai on 2017/9/17.
+ */
+public class MyClient {
+
+    public static void main(String[] args) throws InterruptedException {
+        EventLoopGroup eventLoopGroup = new NioEventLoopGroup();
+        try {
+            Bootstrap bootstrap = new Bootstrap();
+            bootstrap.group(eventLoopGroup).channel(NioSocketChannel.class).handler(new MyClientInitializer());
+            ChannelFuture future = bootstrap.connect("localhost", 8899).sync();
+            future.channel().closeFuture().sync();
+        } finally {
+            eventLoopGroup.shutdownGracefully();
+        }
+    }
+}
---
+++ src/main/java/com/hotech/second/example/MyServerHandler.java
@@ -0,0 +1,24 @@
+package com.hotech.second.example;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+import java.util.UUID;
+
+/**
+ * Created by hekai on 2017/9/17.
+ */
+public class MyServerHandler extends SimpleChannelInboundHandler<String>{
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {
+        System.out.println(ctx.channel().remoteAddress() + ", " + msg);
+        ctx.channel().writeAndFlush("from server" + UUID.randomUUID());
+
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
