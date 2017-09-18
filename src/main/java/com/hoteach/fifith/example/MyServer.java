2664dcfa93ed2c2d97e5ab5157ea20af9a40b79d
hekai <hekai0118@gmail.com>
2017-09-18 21:19:52 +0800

    websocket demo

---
+++ src/main/java/com/hoteach/fifith/example/MyServer.java
@@ -0,0 +1,34 @@
+package com.hoteach.fifith.example;
+
+import com.hoteach.fourth.example.MyServerInitializer;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
+import java.net.InetSocketAddress;
+
+/**
+ * @author hekai
+ * @create 2017-09-18-20:12
+ */
+public class MyServer {
+    public static void main(String[] args) throws InterruptedException {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(bossGroup, workerGroup).channel(NioServerSocketChannel.class).handler(new
+                    LoggingHandler(LogLevel.INFO)).childHandler(new MyServerInitializer());
+
+            ChannelFuture future = serverBootstrap.bind(new InetSocketAddress(8899)).sync();
+            future.channel().closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
