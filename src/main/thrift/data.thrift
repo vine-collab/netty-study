20b1686382a7355353eaf8fd4276d9f8cf5bbd7a
hekai <hekai0118@gmail.com>
2017-10-02 21:48:48 +0800

    thrift idl

---
+++ src/main/thrift/data.thrift
@@ -0,0 +1,25 @@
+namespace java thrift.generated
+
+typedef i16 short
+typedef i32 int
+typedef i64 long
+typedef bool boolean
+typedef string String
+
+struct Person {
+    1:optional String username,
+    2:optional int age,
+    3:optional boolean married
+}
+
+exception DataException {
+    1:optional String message,
+    2:optional String callStack,
+    3:optional String data
+}
+
+service PersonService {
+    Person getPersonByUsername(1:required String username) throws (1:DataException DataException),
+
+    void savePerson(1:required Person person) throws (1:DataException dataException)
+}
