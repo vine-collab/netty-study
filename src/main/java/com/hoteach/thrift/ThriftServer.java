9a1aeb866f3dfa98f0204ff4d430f2d20209a5ca
hekai <hekai0118@gmail.com>
2017-10-04 10:58:08 +0800

    thrift demo is completed

---
+++ src/main/java/com/hoteach/thrift/ThriftServer.java
@@ -0,0 +1,33 @@
+package com.hoteach.thrift;
+
+import org.apache.thrift.TProcessorFactory;
+import org.apache.thrift.protocol.TCompactProtocol;
+import org.apache.thrift.server.THsHaServer;
+import org.apache.thrift.server.TServer;
+import org.apache.thrift.transport.TFramedTransport;
+import org.apache.thrift.transport.TNonblockingServerSocket;
+import org.apache.thrift.transport.TTransportException;
+import thrift.generated.PersonService;
+
+/**
+ * @author hekai
+ * @create 2017-10-04-10:22
+ */
+public class ThriftServer {
+    public static void main(String[] args) throws TTransportException {
+        TNonblockingServerSocket socket = new TNonblockingServerSocket(8899);
+        THsHaServer.Args arg = new THsHaServer.Args(socket).minWorkerThreads(2).maxWorkerThreads(4);
+        PersonService.Processor<PersonServiceImpl> processor = new PersonService.Processor<>(new PersonServiceImpl());
+
+        arg.protocolFactory(new TCompactProtocol.Factory());
+        arg.transportFactory(new TFramedTransport.Factory());
+        arg.processorFactory(new TProcessorFactory(processor));
+
+        TServer server = new THsHaServer(arg);
+
+        System.out.println("Thrift Server Started!");
+
+        server.serve();
+    }
+
+}
