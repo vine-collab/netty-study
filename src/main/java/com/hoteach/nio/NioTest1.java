201d2ab36df734e92d5a2495d6ec06f244ce6a9d
hekai <hekai0118@gmail.com>
2017-10-15 22:03:59 +0800

    Depth nio

--- src/main/java/com/hoteach/nio/NioTest1.java
+++ src/main/java/com/hoteach/nio/NioTest1.java
@@ -13,12 +13,24 @@ public class NioTest1 {
 
     public static void main(String[] args) {
         IntBuffer intBuffer = IntBuffer.allocate(10);
+        System.out.println("capacity:" + intBuffer.capacity());
         for (int i = 0; i < 10; i++){
             int randomNumber = new SecureRandom().nextInt(20);
             intBuffer.put(randomNumber);
         }
+
+        System.out.println("before flip limit:" + intBuffer.limit());
+
         intBuffer.flip();
+
+        System.out.println("after flip limit:" + intBuffer.limit());
+
+        System.out.println("enter while loop");
+
         while (intBuffer.hasRemaining()){
+            System.out.println("position:" + intBuffer.position());
+            System.out.println("limit:" + intBuffer.limit());
+            System.out.println("capacity:" + intBuffer.capacity());
             System.out.println(intBuffer.get());
         }
     }
