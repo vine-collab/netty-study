933a06865b7b928d925b4afb50a90564a86e70f6
hekai <hekai0118@gmail.com>
2017-10-29 10:37:26 +0800

    selector demo

---
+++ src/main/java/com/hoteach/nio/NioTest12.java
@@ -0,0 +1,94 @@
+package com.hoteach.nio;
+
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * @author hekai
+ * @create 2017-10-22-15:14
+ */
+public class NioTest12 {
+    public static void main(String[] args) throws Exception {
+        int[] ports = new int[5];
+        for (int i = 0; i < ports.length; i++) {
+            ports[i] = 5000 + i;
+        }
+        Selector selector = Selector.open();
+
+        for (int port : ports) {
+            ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+            serverSocketChannel.configureBlocking(false);
+            ServerSocket serverSocket = serverSocketChannel.socket();
+            InetSocketAddress address = new InetSocketAddress(port);
+            serverSocketChannel.bind(address);
+            serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+            System.out.println("监听端口：" + port);
+        }
+
+        while (true){
+            int numbers = selector.select();
+            System.out.println("numbers:" + numbers);
+            Set<SelectionKey> selectionKeys = selector.selectedKeys();
+            System.out.println("selectionKeys" + selectionKeys);
+            Iterator<SelectionKey> iterator = selectionKeys.iterator();
+            while (iterator.hasNext()){
+                SelectionKey selectionKey = iterator.next();
+                if(selectionKey.isAcceptable()){
+                    ServerSocketChannel serverSocketChannel = (ServerSocketChannel) selectionKey.channel();
+                    SocketChannel socketChannel = serverSocketChannel.accept();
+                    socketChannel.register(selector, SelectionKey.OP_READ);
+                    iterator.remove();
+                    System.out.println("获得客户端的连接：" + socketChannel);
+                }
+            }
+        }
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
