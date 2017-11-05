e5347ecd5e71bb822bd75dfd28781cfd8fb75cfe
hekai <hekai0118@gmail.com>
2017-11-05 21:20:56 +0800

    NioServer demo

--- src/main/java/com/hoteach/nio/NioServer.java
+++ src/main/java/com/hoteach/nio/NioServer.java
@@ -1,9 +1,97 @@
 package com.hoteach.nio;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.nio.ByteBuffer;
+import java.nio.channels.*;
+import java.nio.charset.Charset;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
+
 /**
  * @author hekai
  * @create 2017-11-05-11:37
  */
 public class NioServer {
 
+    private static Map<String, SocketChannel> clientMap = new HashMap<>();
+
+    public static void main(String[] args) throws IOException {
+        ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+        serverSocketChannel.configureBlocking(false);
+        ServerSocket serverSocket = serverSocketChannel.socket();
+        serverSocket.bind(new InetSocketAddress(8899));
+
+        Selector selector = Selector.open();
+        serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+        while (true) {
+            selector.select();
+            Set<SelectionKey> selectionKeys = selector.selectedKeys();
+            selectionKeys.forEach(item -> {
+                final SocketChannel client;
+                try {
+                    if (item.isAcceptable()) {
+                        ServerSocketChannel server = (ServerSocketChannel) item.channel();
+                        client = server.accept();
+                        client.configureBlocking(false);
+                        client.register(selector, SelectionKey.OP_READ);
+
+                        String key = UUID.randomUUID().toString();
+                        clientMap.put(key, client);
+                    } else if(item.isReadable()){
+                       client =  (SocketChannel)item.channel();
+                        ByteBuffer readBuffer = ByteBuffer.allocate(1024);
+                        int count = client.read(readBuffer);
+                        if(count > 0){
+                            readBuffer.flip();
+                            Charset charset = Charset.forName("utf-8");
+                            String receivedMessage = String.valueOf(charset.decode(readBuffer).array());
+                            System.out.println(client + ":" + receivedMessage);
+
+
+                        }
+                        
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main/java/com/hoteach/nio/NioTest12.java
+++ src/main/java/com/hoteach/nio/NioTest12.java
@@ -1,7 +1,11 @@
 package com.hoteach.nio;
 
+import io.netty.buffer.ByteBuf;
+
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
@@ -32,21 +36,37 @@ public class NioTest12 {
             System.out.println("监听端口：" + port);
         }
 
-        while (true){
+        while (true) {
             int numbers = selector.select();
             System.out.println("numbers:" + numbers);
             Set<SelectionKey> selectionKeys = selector.selectedKeys();
             System.out.println("selectionKeys" + selectionKeys);
             Iterator<SelectionKey> iterator = selectionKeys.iterator();
-            while (iterator.hasNext()){
+            while (iterator.hasNext()) {
                 SelectionKey selectionKey = iterator.next();
-                if(selectionKey.isAcceptable()){
+                if (selectionKey.isAcceptable()) {
                     ServerSocketChannel serverSocketChannel = (ServerSocketChannel) selectionKey.channel();
                     SocketChannel socketChannel = serverSocketChannel.accept();
                     socketChannel.configureBlocking(false);
                     socketChannel.register(selector, SelectionKey.OP_READ);
                     iterator.remove();
                     System.out.println("获得客户端的连接：" + socketChannel);
+                } else if (selectionKey.isReadable()) {
+                    SocketChannel socketChannel = (SocketChannel) selectionKey.channel();
+                    int bytesRead = 0;
+                    while (true){
+                        ByteBuffer byteBuffer = ByteBuffer.allocate(512);
+                        byteBuffer.clear();
+                        int read = socketChannel.read(byteBuffer);
+                        if(read <= 0){
+                            break;
+                        }
+                        byteBuffer.flip();
+                        socketChannel.write(byteBuffer);
+                        bytesRead += read;
+                    }
+                    System.out.println("读取：" + bytesRead + ", 来自于：" +socketChannel);
+                    iterator.remove();
                 }
             }
         }
